4d147ece643acf32a1e79164c53eff3138a7e4df
Denis Medvedev <medvedev.denision@gmail.com>
2024-04-20 14:35:40 +0300

    init-project

---
+++ life-index/src/main/java/spb/hack/lifeindex/LifeIndexApplication.java
@@ -0,0 +1,13 @@
+package spb.hack.lifeindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeIndexApplication.class, args);
+    }
+
+}
---
+++ life-index/src/test/java/spb/hack/lifeindex/LifeIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package spb.hack.lifeindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

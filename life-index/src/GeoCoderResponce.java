0648c6e2e5967ed439d9ccb70d2892e66d88b772
Qusild <74545363+Qusild@users.noreply.github.com>
2024-04-20 15:24:38 +0300

    GeoCoder request template

---
+++ life-index/src/GeoCoderResponce.java
@@ -0,0 +1,9 @@
+public class GeoCoderResponce {
+    public GeoCoderResponce(String jwtToken){ this._jwtToken = jwtToken; }
+    public GeoCoderResponce getCoordinates(String address) {
+        GeoCoderInformation geoInformation = new GeoCoderInformation();
+        //AHAHAHAHAHAAHAH
+        return GeoInformation;
+    }
+    private String _jwtToken;
+}

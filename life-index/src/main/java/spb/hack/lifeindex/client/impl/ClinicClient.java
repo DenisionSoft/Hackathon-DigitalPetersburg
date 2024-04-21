136244a8f3709444bbc6a9e58ea230dbc57c4830
Denis Medvedev <medvedev.denision@gmail.com>
2024-04-21 14:05:40 +0300

    Clinic added

--- life-index/src/main/java/spb/hack/lifeindex/client/impl/ClinicClient.java
+++ life-index/src/main/java/spb/hack/lifeindex/client/impl/ClinicClient.java
@@ -1,6 +1,7 @@
 package spb.hack.lifeindex.client.impl;
 
 import lombok.AllArgsConstructor;
+import org.antlr.v4.runtime.misc.Pair;
 import org.springframework.stereotype.Service;
 import spb.hack.lifeindex.client.ApiClient;
 import spb.hack.lifeindex.client.ProxyClient;
@@ -8,8 +9,12 @@ import spb.hack.lifeindex.model.District;
 import spb.hack.lifeindex.model.GeoPoint;
 import spb.hack.lifeindex.model.House;
 import spb.hack.lifeindex.model.dto.RequestParamsDto;
+import spb.hack.lifeindex.model.dto.impl.ClinicDto;
 import spb.hack.lifeindex.model.dto.impl.GeocoderDto;
+import spb.hack.lifeindex.model.response.ClinicResponse;
 import spb.hack.lifeindex.model.response.GeocoderResponse;
+
+import java.util.ArrayList;
 
 @Service
 @AllArgsConstructor
@@ -19,27 +24,24 @@ public class ClinicClient implements ApiClient {
     private static final String BASE_URL = "https://spb-classif.gate.petersburg.ru/api/v2/datasets/149/versions/latest/data/327";
 
     @Override
-    public GeocoderDto getDto(RequestParamsDto requestParamsDto) {
-        /*
+    public ClinicDto getDto(RequestParamsDto requestParamsDto) {
         // подготовка урла
-        String endpoint = "";
-        String paramPage = "page=
-        String url = BASE_URL + endpoint + paramStreet;
+        String endpoint = "?";
+        String paramPage = "page=" + requestParamsDto.getPage();
+        String paramPerPage = "&per_page=100";
+        String url = BASE_URL + endpoint + paramPage + paramPerPage;
         // запрос к прокси клиенту
-        GeocoderResponse geocoderResponse = proxyClient.get(url, GeocoderResponse.class);
+        ClinicResponse clinicResponse = proxyClient.get(url, ClinicResponse.class);
         // обработка респонса в дто
-        District district = new District(geocoderResponse.districtName, geocoderResponse.districtId);
-        GeoPoint geoPoint = new GeoPoint(geocoderResponse.latitude, geocoderResponse.longitude);
-        House house = new House(geocoderResponse.buildingId, geocoderResponse.address, district, geoPoint);
-
-        GeocoderDto geocoderDto = new GeocoderDto();
-        geocoderDto.setDistrict(district);
-        geocoderDto.setGeoPoint(geoPoint);
-        geocoderDto.setHouse(house);
+        Integer count = clinicResponse.getResults().size();
+        ArrayList<Pair<GeoPoint, District>> results = new ArrayList<>();
+        clinicResponse.getResults().forEach(result -> {
+            results.add(new Pair<>(new GeoPoint(result.getCoordinates().get(0), result.getCoordinates().get(1)), new District(result.getDistrict())));
+        });
+        ClinicDto clinicDto = new ClinicDto();
+        clinicDto.setCount(count);
+        clinicDto.setResults(results);
         // возврат дто
-        return geocoderDto;
-
-         */
-        return null;
+        return clinicDto;
     }
 }
